8caf8f590410ee22145e606b013edbad34ccd1fd
roccomarco <roccomarco@b1d1f220-3f59-4ac7-8100-3e80b4e6ba87>
2017-06-10 15:00:39 +0000

    Removed old branch

--- PEdemos161/RT-STM32F401RE-NUCLEO-NRF24L01-String/userlib/user.mk
+++
@@ -1,7 +0,0 @@
-USERLIB = ./userlib
-# List of all the Userlib files
-USERSRC =  $(USERLIB)/src/rf.c 
-          
-# Required include directories
-USERINC =  $(USERLIB) \
-           $(USERLIB)/include 
