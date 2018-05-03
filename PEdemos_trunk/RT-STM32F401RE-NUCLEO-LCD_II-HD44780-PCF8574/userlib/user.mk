b13bb29fa62ec215ae4d1d51e6eaec3b86837fae
roccomarco <roccomarco@b1d1f220-3f59-4ac7-8100-3e80b4e6ba87>
2018-05-03 08:50:36 +0000

    Removed unproper folders

--- PEdemos_trunk/RT-STM32F401RE-NUCLEO-LCD_II-HD44780-PCF8574/userlib/user.mk
+++
@@ -1,7 +0,0 @@
-USERLIB = ./userlib
-# List of all the Userlib files
-USERSRC =  $(USERLIB)/src/lcd.c 
-          
-# Required include directories
-USERINC =  $(USERLIB) \
-           $(USERLIB)/include 
